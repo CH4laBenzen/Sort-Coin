a30d90bd9a0752bfe8a22c0d8994e8cd6731cb58
ThanhTung <highntt@gmail.com>
2026-04-03 10:11:35 +0700

    Initial commit

---
+++ Assets/Scripts/Help/Constain.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MainGame
+{
+    public static class Constain
+    {
+        public const string IsPlaySFX = "SFXVolume";
+        public const string IsPlayMusic = "MusicVolume";
+        public const string IsHaptic = "IsHaptic";
+
+        public const string Player = "Player";
+        public const string Idle = "Idle";
+        public const string Work = "Work";
+        public const string OpenUI = "Open";
+        public const string OpenUI2 = "Open2";
+        public const string CloseUI = "Close";
+        public const string Angry = "Angry";
+        public const string Happy = "Happy";
+        
+
+        public const string Open = "Open";
+        public const string Close = "Close";
+        public const string Push = "Push";
+        public const string OpenBox = "OpenBox";
+        public const string CloseBox = "CloseBox";
+
+        public const string animation = "animation";
+
+        public const string Heart = "Heart";
+        public const string Coin = "Coin";
+
+        public const string WinStreak = "WinStreak";
+        public const string WinChess = "WinChess";
+        public const string OverallGem = "OverallGem";
+        public const string OverallCoin = "OverallCoin";
+        public const string PiggyBankCoin = "PiggyBankCoin";
+        public const string Stars = "Stars";
+        public const string LevelCompleted = "LevelCompleted";
+        public const string LevelUnlockIndex = "LevelUnlockIndex";
+
+        public const string CurrentHouseIndex = "CurrentHouseIndex";
+        public const string CurrentLevelIndex = "CurrentLevelIndex";
+
+        public const string HouseCompleted = "HouseCompleted";
+        public const string HouseUnlock = "HouseUnlock";
+
+        public const string LevelUnlock = "LevelUnlock";
+        public const string LevelStar = "LevelStar";
+
+        public const string LastHeartTime = "LastHeartTime";
+        public const string LastLoginDate = "LastLoginDate";
+        public const string LastClaimLogin = "LastLoginDate";
+
+        public const string IsClaimMissionChest = "IsClaimMissionChest";
+
+        public const string CurrentLanguageIndex = "CurrentLanguageIndex";
+
+        public const string IsInfinity = "IsInfinity";
+        public const string InfinityStartTime = "InfinityStartTime";
+        public const string InfinityDuration = "InfinityDuration";
+
+        public const string IsRateApp = "IsRateApp";
+        public const string IsTutorial = "IsTutorial";
+
+        public const string LastSpinDateKey = "LastSpinDate";
+        public const string SpinCountKey = "SpinCountToday";
+        public const string FirstSpinKey = "IsFirstSpin";
+
+        public const string KeyLevelUnlock = "keyLevelUnlock_ColorBlockJam";
+
+        public const string RemoveAds = "RemoveAds";
+        public const string BoughtStartedPack = "BoughtStartedPack";
+        public const string BoughtBeginnerPack = "BoughtBeginnerPack";
+        public const string UnlockVipPack = "UnlockVipPack";
+        public const string VipPackStartTime = "VipPackStartTime";
+
+        public const string VIPPack1 = "VIPPack1";
+        public const string VIPPack2 = "VIPPack2";
+        public const string VIPPack3 = "VIPPack3";
+
+        public const string IsAskNotify = "IsAskNotify";
+
+
+        public const string BagTutorial = "BagTutorial";
+        public const string HiddenBagTutorial = "HiddenBagTutorial";
+        public const string FrostBagTutorial = "FrostBagTutorial";
+        public const string NewJourney = "NewJourney";
+        public const string StackTutorial = "StackTutorial";
+        public const string CoupleBagTutorial = "CoupleBagTutorial";
+        public const string LockAndKeyTutorial = "LockAndKeyTutorial";
+        public const string ConveyorTutorial = "ConveyorTutorial";
+
+        public const string AvatarIndex = "AvatarIndex";
+        public const string UserName = "UserName";
+
+
+    }
+}
+   
---
+++ Assets/Scripts/Help/EventManager.cs
@@ -0,0 +1,135 @@
+﻿using MainGame;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class GameEvent { }
+namespace MainGame
+{
+    public static class EventManager
+    {
+        static readonly Dictionary<Type, Action<GameEvent>> s_Events = new Dictionary<Type, Action<GameEvent>>();
+
+        static readonly Dictionary<Delegate, Action<GameEvent>> s_EventLookups =
+            new Dictionary<Delegate, Action<GameEvent>>();
+
+        public static void AddListener<T>(Action<T> evt) where T : GameEvent
+        {
+            if (!s_EventLookups.ContainsKey(evt))
+            {
+                Action<GameEvent> newAction = (e) => evt((T)e);
+                s_EventLookups[evt] = newAction;
+                if (s_Events.TryGetValue(typeof(T), out Action<GameEvent> internalAction))
+                    s_Events[typeof(T)] = internalAction += newAction;
+                else
+                    s_Events[typeof(T)] = newAction;
+            }
+        }
+
+        public static void RemoveListener<T>(Action<T> evt) where T : GameEvent
+        {
+            if (s_EventLookups.TryGetValue(evt, out var action))
+            {
+                if (s_Events.TryGetValue(typeof(T), out var tempAction))
+                {
+                    tempAction -= action;
+                    if (tempAction == null)
+                        s_Events.Remove(typeof(T));
+                    else
+                        s_Events[typeof(T)] = tempAction;
+                }
+
+                s_EventLookups.Remove(evt);
+            }
+        }
+
+        public static void Trigger(GameEvent evt)
+        {
+            if (s_Events.TryGetValue(evt.GetType(), out var action))
+                action.Invoke(evt);
+        }
+
+        public static void Clear()
+        {
+            s_Events.Clear();
+            s_EventLookups.Clear();
+        }
+    }
+
+    public class OnCoinAdd : GameEvent
+    {
+        public int CoinAmount;
+    }
+
+    public class OnCoinPay : GameEvent
+    {
+        public int PayAmount;
+    }
+    public class OnNotice : GameEvent
+    {
+        public string Content;
+    }
+    public class OnStarLevel : GameEvent { }
+
+
+    public class OnCompleteLevel : GameEvent
+    {
+        public int CustomerServedAmount;
+    }
+
+    public class OnLevelClose : GameEvent
+    {
+        public int CustomerServedAmount;
+    }
+
+    public class OnWatchAds : GameEvent { }
+    public class SendInterData : GameEvent
+    {
+        public string interPlacement;
+        public string levelInter;
+    }
+
+    public class SendRewardData : GameEvent
+    {
+        public string levelReward;
+        public string button_name;
+        public string reward_name;
+        public string reward_type;
+        public int value;
+        public string rewardPlacement;
+    }
+
+    public class OnUseBooster : GameEvent { }
+    public class OnCompleteLuckSpin : GameEvent { }
+
+    public class OnIAPPurchase : GameEvent
+    {
+        public Product Product;
+        public int Level;
+        public string Pack_id;
+        public string Pack_name;
+        public string Placement;
+    }
+
+    public class OnBagClick : GameEvent { }
+
+    public class OnCustomerServed : GameEvent { }
+
+    public class OnConveyorDone : GameEvent { }
+
+    public class OnTrayOpened : GameEvent
+    {
+        public WaitingTray OpenedTray; // Khay vừa mở
+        public Vector3 Position;
+    }
+
+    public class OnPackVipPurchase : GameEvent { }
+    public class OnPackVipUnlock : GameEvent { }
+
+
+    public class OnBagBoxDone : GameEvent
+    {
+        public BagBox BagBox;
+    }
+}
